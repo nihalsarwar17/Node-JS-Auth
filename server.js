eed1ade921464ab3eb72cf42ea245a0a9bad401b
Nihal Sarwar <nihalsarwar17@gmail.com>
2022-08-29 02:33:52 +0500

    authenticate user crendentials

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ const ObjectId = require("mongodb").ObjectId;
 const URI = "mongodb://localhost:27017/AdvancedNode";
 const mongo = require("mongodb").MongoClient; // mongo connects with the databases
 
+app.set("view engine", "pug");
+app.set("views", "./pages");
+
 app.use(
   session({
-    secret: "sdufg", // cookie secret key
-    resave: true, // store session ID
-    saveUninitialized: true, // creates and stores session object
+    secret: "sdufg",
+    resave: true,
+    saveUninitialized: true,
   }),
   passport.initialize(),
   passport.session()
 );
 
-app.set("view engine", "pug");
-app.set("views", "./pages");
-
+// connect with the database
 mongo.connect(URI, (error, client) => {
   if (error) {
     console.log(error);
@@ -34,31 +35,40 @@ mongo.connect(URI, (error, client) => {
     });
 
     // *********************** ROUTES ****************************
-    app.get("/", (req, res) => {
-      console.log(req.session);
-      res.render("login", { message: "Please sign in to proceed" });
+    app.get("/", (request, res) => {
+      request.session.count++; // every time page reloads, it increments the count
+      console.log(request.session);
+
+      res.render("login", {
+        website_msg: "FanClub is excited to have you with us!",
+      });
     });
 
-    app.get("/profile", (req, res) => {
-      console.log(req.session);
-      res.render("user", { message: "Lets start exploring" });
-    }); 
-
-    app.get("/failure", (req, res) => {
-      console.log(req.session);
-      res.render("failure", { message: "Invalid Credentials, Please Login again" });
+    app.get("/failure", (request, resp) => {
+      resp.render("failure", {
+        website_msg: "Try again to login!",
+      });
     });
 
-    // *********************** ROUTES ENDED **********************
+    app.get("/profile", function (req, res, next) {
+      res.render("user", { userName: req.user.name, image: req.user.picture });
+    });
 
-    // Save User ID in a cookie
-    // user is the mongoDB collection where users data is stored
+    // *********************** ROUTES ENDED ****************************
+
+    // ================= store User ID in cookie ===================
+
+    // save User ID in a cookies
+    // user => stores all properties of user data
+    // done => determine what user data should store in the cookie from the user object
+
     passport.serializeUser((user, done) => {
       done(null, user._id);
     });
 
-    // take user ID from cookie and retrieve user's data
+    // Retrieve User ID from the cookie
     passport.deserializeUser((userId, done) => {
+      // open 'users' named collection in database and
       db.collection("users").findOne(
         // userId retrieves the ID from the cookie and assign it to an object
         // so it could find the user details from the collection to its corresponding ID
@@ -72,18 +82,21 @@ mongo.connect(URI, (error, client) => {
       );
     });
 
-    let findUserDocument = new LocalStrategy((username, password, done) => {
+    const findUserDocument = new LocalStrategy((username, password, done) => {
       db.collection("users").findOne(
         { username: username },
-        // user contains id & password
+        // user has all the info of the user
         (err, user) => {
           if (err) {
             return done(err);
-          } else if (!user) {
-            // if user doesn't exist
-            done(null, false); // no user found
-          } else if (user.username !== password) {
-            done(null, false); // invalid password
+          }
+          // if user is not there
+          else if (!user) {
+            // send authentication error
+            done(null, false);
+          } else if (user.password !== password) {
+            // send authentication error
+            done(null, false);
           } else {
             done(null, user);
           }
@@ -91,9 +104,15 @@ mongo.connect(URI, (error, client) => {
       );
     });
 
-    // authenticates users login crendentials
+    // take username and password and verifies them and return the user info
     passport.use(findUserDocument);
-    
+
+    app.use(
+      bodyParser.urlencoded({
+        extended: false,
+      })
+    );
+
     app.post(
       "/user/submit",
       passport.authenticate("local", { failureRedirect: "/failure" }),
@@ -103,5 +122,19 @@ mongo.connect(URI, (error, client) => {
         res.redirect("/profile");
       }
     );
-  } // else bracket
+
+    // let isSignedIn = (req, res, next) => {
+    //   if (req.isAuthenticated()) {
+    //     // if user signed in
+    //     next();
+    //   } else {
+    //     // if user doesn't signed in
+    //     res.redirect("/");
+    //   }
+    // };
+
+    // app.get("/profile", isSignedIn, function (req, res, next) {
+    //   res.render("user", { userName: req.user.name, image: req.user.picture });
+    // });
+  }
 });
