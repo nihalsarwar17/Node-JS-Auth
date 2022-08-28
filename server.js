9dfb3afea203f7b5824953803b3dd5ad78c5d1a4
Nihal Sarwar <nihalsarwar17@gmail.com>
2022-08-29 01:47:31 +0500

    connected to MongoDB

--- server.js
+++ server.js
@@ -10,11 +10,54 @@ const ObjectId = require("mongodb").ObjectId;
 const URI = "mongodb://localhost:27017/AdvancedNode";
 const mongo = require("mongodb").MongoClient; // mongo connects with the databases
 
+app.use(
+  session({
+    secret: "sdufg", // cookie secret key
+    resave: true, // store session ID
+    saveUninitialized: true, // creates and stores session object
+  }),
+  passport.initialize(),
+  passport.session()
+);
+
 app.set("view engine", "pug");
 app.set("views", "./pages");
 
-app.get('/', (req, res)=>{
-  res.render('homepage', {message: "Please sign in to proceed"})
-})
+mongo.connect(URI, (error, client) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const db = client.db("AdvancedNode");
+    app.listen(3000, () => {
+      console.log("server is running on 3000 port");
+      console.log("Connected to the database");
+    });
+    app.get("/", (req, res) => {
+      console.log(req.session);
+      res.render("homepage", { message: "Please sign in to proceed" });
+    });
+    
+    // Save User ID in a cookie
+    // user is the mongoDB collection where users data is stored
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    
+    // take user ID from cookie and retrieve user's data
+    passport.deserializeUser((userId, done) => {
+      db.collection("users").findOne(
+        // userId retrieves the ID from the cookie and assign it to an object
+        // so it could find the user details from the collection to its corresponding ID
+        { _id: new ObjectId(userId) },
+    
+        // once it matches the ID then it retrieves all the user info
+        (error, doc) => {
+          // doc has the detailed info of users
+          done(null, doc);
+        }
+      );
+    });
+  } // else bracket
+});
 
-app.listen(3000, ()=>{
+
